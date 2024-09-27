58a317dcd31ceb511db126c123060ff5c738f910
Lin Yang <geekya215@gmail.com>
2024-09-27 10:08:28 +0800

    add maven dep

---
+++ src/main/java/io/geekya215/meowjudge/MeowJudgeApplication.java
@@ -0,0 +1,13 @@
+package io.geekya215.meowjudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowJudgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeowJudgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/geekya215/meowjudge/MeowJudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.geekya215.meowjudge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeowJudgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
